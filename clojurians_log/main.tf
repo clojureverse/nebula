af5c33c712ab16445e1a003b08ab561274eb58b3
Rahul De <rahul080327@gmail.com>
2019-05-10 11:50:04 +0200

    Start terraform provisioning

--- clojurians_log/main.tf
+++ clojurians_log/main.tf
@@ -4,13 +4,18 @@ provider "exoscale" {
   secret = "${var.exoscale_secret_key}"
 }
 
+data "template_file" "userdata" {
+  template = "${file("userdata.sh.tmpl")}"
+}
+
 resource "exoscale_compute" "clojurians_log" {
   display_name = "clojurians-log"
   template = "Linux Ubuntu 18.04 LTS 64-bit"
   zone = "de-fra-1"
-  size = "Micro"
+  size = "Medium"
   disk_size = 10
   key_pair = "${var.exoscale_ssh_keypair_name}"
+  user_data = "${data.template_file.userdata.rendered}"
 
   security_groups = [
     "${exoscale_security_group.clojurians_log.name}",
