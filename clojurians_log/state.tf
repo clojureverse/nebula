08d1001dee54f3c5aefe6528338cb325e08dbe14
Dorab Patel <dorabpatel@gmail.com>
2019-08-07 19:22:14 -0700

    Remove deprecated S3 params.

--- clojurians_log/state.tf
+++ clojurians_log/state.tf
@@ -7,8 +7,6 @@ terraform {
     endpoint = "https://sos-de-fra-1.exo.io"
 
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_requesting_account_id = true
     skip_metadata_api_check = true
     skip_region_validation = true
   }
